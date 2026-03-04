16e33a2c30b3fe3d9c833a04c61d006b39a3a3b1
Rapha Panchi <raphapanchi@gmail.com>
2026-03-04 18:17:55 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+
+import * as FeedCore from "@illimi/core";
+import * as FeedRuntime from "@illimi/runtime";
+import * as FeedVite from "@illimi/vite";
+
+export { FeedCore, FeedRuntime, FeedVite };
